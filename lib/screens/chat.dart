48f7e3a37bc3325f049006c7cd2ec8b4e6274c62
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-04-06 12:50:36 +0530

    Initial commit of Flutter project

---
+++ lib/screens/chat.dart
@@ -0,0 +1,38 @@
+import 'package:chat_app_n/widgets/chat_messages.dart';
+import 'package:chat_app_n/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        backgroundColor: Colors.cyan.shade200,
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
