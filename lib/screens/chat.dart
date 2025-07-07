98d3addb4cb16dc664c7a44cf852d6d8c61d06c6
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-07-07 23:19:38 +0530

    updated

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -11,7 +11,7 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chat'),
+        title: const Text('Flutter Chat'),
         backgroundColor: Colors.cyan.shade200,
         actions: [
           IconButton(
