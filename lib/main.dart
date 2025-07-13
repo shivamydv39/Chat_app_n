9968ed703ce2acfa1aebff80610bafefc5ff3972
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-07-13 06:34:16 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FlutterChat',
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 63, 17, 177)),
-      ),
+
       theme: ThemeData().copyWith(
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 255, 255, 255)),
